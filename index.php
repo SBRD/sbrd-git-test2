b31fbc87a14122e713fa84657d1c3f5ca03485c1
SBRD <saddan.rachman@gmail.com>
2020-02-20 17:59:09 +0800

    menambahkan ul dan li

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 <body>
 <h1>Hello World</h1>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti animi similique labore cumque perferendis asperiores facilis reiciendis incidunt, pariatur ab consequuntur sunt nulla. Temporibus unde eaque, magnam molestiae reprehenderit in.</p>
-    
+    <ul>
+    <li>aa</li>
+    </ul>
 </body>
 </html>
